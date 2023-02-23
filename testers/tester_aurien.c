1f7c8b31e7b79cb7a1916d8f92570677ec6cd5a8
akadil.kalimoldayev@gmail.com <akadil.kalimoldayev@gmail.com>
2023-02-23 12:34:38 +0100

    message

--- testers/tester_aurien.c
+++ testers/tester_aurien.c
@@ -1,8 +1,9 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
 #include <X11/X.h>
 #include <X11/keysym.h>
 #include <mlx.h>
-#include <stdio.h>
-#include <stdlib.h>
 
 #define WINDOW_WIDTH 600
 #define WINDOW_HEIGHT 300
@@ -10,8 +11,25 @@
 #define MLX_ERROR 1
 
 #define RED_PIXEL 0xFF0000
-#define GREEN_PIXEL 0x00FF00
-#define BLUE_PIXEL 0x0000FF
+#define GREEN_PIXEL 0xFF00
+#define WHITE_PIXEL 0xFFFFFF
+
+typedef struct s_img
+{
+	void	*mlx_img;
+	char	*addr;
+	int		bpp; /* bits per pixel */
+	int		line_len;
+	int		endian;
+}	t_img;
+
+typedef struct s_data
+{
+	void	*mlx_ptr;
+	void	*win_ptr;
+	t_img	img;
+	int		cur_img;
+}	t_data;
 
 typedef struct s_rect
 {
@@ -22,11 +40,59 @@ typedef struct s_rect
 	int color;
 }	t_rect;
 
-typedef struct s_data
+void	img_pix_put(t_img *img, int x, int y, int color)
 {
-	void	*mlx_ptr;
-	void	*win_ptr;
-}	t_data;
+	char    *pixel;
+	int		i;
+
+	i = img->bpp - 8;
+    pixel = img->addr + (y * img->line_len + x * (img->bpp / 8));
+	while (i >= 0)
+	{
+		/* big endian, MSB is the leftmost bit */
+		if (img->endian != 0)
+			*pixel++ = (color >> i) & 0xFF;
+		/* little endian, LSB is the leftmost bit */
+		else
+			*pixel++ = (color >> (img->bpp - 8 - i)) & 0xFF;
+		i -= 8;
+	}
+}
+
+/* The x and y coordinates of the rect corresponds to its upper left corner. */
+
+int render_rect(t_img *img, t_rect rect)
+{
+	int	i;
+	int j;
+
+	i = rect.y;
+	while (i < rect.y + rect.height)
+	{
+		j = rect.x;
+		while (j < rect.x + rect.width)
+			img_pix_put(img, j++, i, rect.color);
+		++i;
+	}
+	return (0);
+}
+
+void	render_background(t_img *img, int color)
+{
+	int	i;
+	int	j;
+
+	i = 0;
+	while (i < WINDOW_HEIGHT)
+	{
+		j = 0;
+		while (j < WINDOW_WIDTH)
+		{
+			img_pix_put(img, j++, i, color);
+		}
+		++i;
+	}
+}
 
 int	handle_keypress(int keysym, t_data *data)
 {
@@ -38,48 +104,15 @@ int	handle_keypress(int keysym, t_data *data)
 	return (0);
 }
 
-/* The x and y coordinates of the rect corresponds to its upper left corner. */
-
-void	render_background(t_data *data, int color)
-{
-	int	i;
-	int	j;
-
-	if (data->win_ptr == NULL)
-		return ;
-	i = 0;
-	while (i < WINDOW_HEIGHT)
-	{
-		j = 0;
-		while (j < WINDOW_WIDTH)
-			mlx_pixel_put(data->mlx_ptr, data->win_ptr, j++, i, color);
-		++i;
-	}
-}
-
-int render_rect(t_data *data, t_rect rect)
-{
-	int	i;
-	int j;
-
-	if (data->win_ptr == NULL)
-		return (1);
-	i = rect.y;
-	while (i < rect.y + rect.height)
-	{
-		j = rect.x;
-		while (j < rect.x + rect.width)
-			mlx_pixel_put(data->mlx_ptr, data->win_ptr, j++, i, rect.color);
-		++i;
-	}
-	return (0);
-}
-
 int	render(t_data *data)
 {
-    render_background(data, BLUE_PIXEL);
-    render_rect(data, (t_rect){WINDOW_WIDTH - 100, WINDOW_HEIGHT - 100, 100, 100, GREEN_PIXEL});
-	render_rect(data, (t_rect){0, 0, 100, 100, RED_PIXEL});
+	if (data->win_ptr == NULL)
+		return (1);
+	render_background(&data->img, WHITE_PIXEL);
+	render_rect(&data->img, (t_rect){WINDOW_WIDTH - 100, WINDOW_HEIGHT - 100, 100, 100, GREEN_PIXEL});
+	render_rect(&data->img, (t_rect){0, 0, 100, 100, RED_PIXEL});
+
+	mlx_put_image_to_window(data->mlx_ptr, data->win_ptr, data->img.mlx_img, 0, 0);
 
 	return (0);
 }
@@ -91,8 +124,7 @@ int	main(void)
 	data.mlx_ptr = mlx_init();
 	if (data.mlx_ptr == NULL)
 		return (MLX_ERROR);
-	data.win_ptr = mlx_new_window(data.mlx_ptr, WINDOW_WIDTH, WINDOW_HEIGHT,
-								"my window");
+	data.win_ptr = mlx_new_window(data.mlx_ptr, WINDOW_WIDTH, WINDOW_HEIGHT, "my window");
 	if (data.win_ptr == NULL)
 	{
 		free(data.win_ptr);
@@ -100,12 +132,18 @@ int	main(void)
 	}
 
 	/* Setup hooks */ 
+	data.img.mlx_img = mlx_new_image(data.mlx_ptr, WINDOW_WIDTH, WINDOW_HEIGHT);
+	
+	data.img.addr = mlx_get_data_addr(data.img.mlx_img, &data.img.bpp,
+			&data.img.line_len, &data.img.endian);
+
 	mlx_loop_hook(data.mlx_ptr, &render, &data);
 	mlx_hook(data.win_ptr, KeyPress, KeyPressMask, &handle_keypress, &data);
 
 	mlx_loop(data.mlx_ptr);
 
 	/* we will exit the loop if there's no window left, and execute this code */
+	mlx_destroy_image(data.mlx_ptr, data.img.mlx_img);
 	mlx_destroy_display(data.mlx_ptr);
 	free(data.mlx_ptr);
 }
